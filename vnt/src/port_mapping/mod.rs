3f7cbc50b1fec3c1d7def386543244d3414e9b56
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2025-11-22 10:21:54 +0800

    调整运行时

--- vnt/src/port_mapping/mod.rs
+++ vnt/src/port_mapping/mod.rs
@@ -75,10 +75,10 @@ pub fn start_port_mapping(
         .name("portMapping".into())
         .spawn(move || {
             let runtime = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .thread_name("portMapping")
-            .build()?;
-            runtime.block_on(start_port_mapping0(vec))?;
+                .enable_all()
+                .thread_name("portMapping")
+                .build().unwrap();
+            runtime.block_on(start_port_mapping0(vec)).unwrap();
             runtime.block_on(async {
                 let _ = receiver.await;
             });
